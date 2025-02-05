824e4ba73f90bdee64b8ae0c7c809b1ccdfaa674
Moritz Huber <moritz@Moritzs-MacBook-Pro.local>
2025-02-05 23:14:27 +0100

    first commit

---
+++ src/test.c
@@ -0,0 +1,24 @@
+#include "int-list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    List* arglist = list_create();
+    printf("List empty: %d\n", list_empty(arglist));
+    for (int i = 1; i < argc; i++) {
+        /* if (!list_prepend(arglist, argv[i])) { */
+        /*     printf("error list_prepend\n"); */
+        /*     return 1; */
+        /* } */
+        if (!list_append(arglist, atoi(argv[i]))) {
+            printf("error list_prepend\n");
+            return 1;
+        }
+    }
+    printf("List empty: %d\n", list_empty(arglist));
+    printf("List count: %d\n", list_count(arglist));
+    list_print(arglist);
+    list_destroy(arglist);
+    return 0;
+}
